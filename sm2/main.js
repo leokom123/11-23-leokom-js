3eb6412f762a1b51288bdf5a0ec089f15d8b5c2d
Комбаров Леонид Андреевич <152315@mck-ktits.ru>
2024-09-21 09:33:14 +0300

    sm2

---
+++ sm2/main.js
@@ -0,0 +1,14 @@
+let a = prompt("как тебя зовут?");
+let b = prompt("сколько тебе лет?");
+
+if (b < 20){
+alert("тебе меньше 20")
+}else{(b >= 20)
+    for (let i = 0; i < 5; i++){
+    alert("привет");
+}
+}
+let c = confirm("хочешь учить js?")
+if (ok){
+alert("good")
+}
