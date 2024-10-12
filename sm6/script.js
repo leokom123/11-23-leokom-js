feb58b26422d9bd4afe736848ff2e5bdd09ba72c
leokom123 <kombarovleonid099@gmail.com>
2024-10-12 16:59:10 +0300

    Add files via upload

---
+++ sm6/script.js
@@ -0,0 +1,6 @@
+document.getElementById("op_btn").addEventListener("click",function(){
+    document.getElementById("123").classList.add("open")
+})
+document.getElementById("333").addEventListener("click",function(){
+    document.getElementById("123").classList.remove("open")
+})
