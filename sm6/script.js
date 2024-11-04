dde5a1c54dfce03537f5eb787b26fdb4d37386d9
Kombarov Leonid <leonidkokombar@gmail.com>
2024-11-04 16:26:49 +0300

    Revert "Add files via upload"
    This reverts commit feb58b26422d9bd4afe736848ff2e5bdd09ba72c.

--- sm6/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("op_btn").addEventListener("click",function(){
-    document.getElementById("123").classList.add("open")
-})
-document.getElementById("333").addEventListener("click",function(){
-    document.getElementById("123").classList.remove("open")
-})
