7bee556ca237733666ed1433789a6680146ee548
leokom123 <kombarovleonid099@gmail.com>
2024-11-30 08:36:22 +0300

    Add files via upload

---
+++ sm3/main.js
@@ -0,0 +1,30 @@
+let a = prompt("как тебя зовут?");
+let b = prompt("сколько тебе лет?");
+
+if (b < 20){
+alert("тебе меньше 20")
+}else{(b >= 20)
+    for (let i = 0; i < 5; i++){
+    alert("привет");
+}
+}
+let c = confirm("хочешь учить js?")
+if (c){
+alert("good")
+}
+
+function sum(a, b){
+    return a + b
+}
+alert(sum(5, 1));
+
+function v(a, b){
+    return a - b
+}
+alert(v(5, 1));
+
+function sum1(a, b) {
+    alert(a +b);
+}
+
+sum1(5, 1);
